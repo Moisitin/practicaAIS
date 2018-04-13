239e61e08a5ede21456bfd7310199251e9566227
[pstfe] <[paula_sz@hotmail.es]>
2018-04-13 11:49:53 +0200

    hola

--- Buscaminas/src/buscaminas/Buscaminas.java
+++ Buscaminas/src/buscaminas/Buscaminas.java
@@ -2,7 +2,7 @@ package Buscaminas;
 
 import java.awt.*;
 import java.awt.event.*;
- 
+ //holllaaa
 import javax.swing.*;
 public class Buscaminas extends JFrame implements ActionListener, MouseListener{
     System.out.println('se lia');
